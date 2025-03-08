701e993225b4be1c5f7bb4638fb4999cec3e0806
keno <keno.obuwoma@gmail.com>
2025-03-07 21:23:20 -0500

    construction

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+}
